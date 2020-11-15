42def63144cbc3fc3ff473af89ef05b01a51c2eb
LyanAzidie <30411767+LyanAzidie@users.noreply.github.com>
2020-11-15 17:12:49 +1100

    added food

---
+++ Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SplashKitSDK;
+
+namespace Snake
+{
+    public class Food : GameObject
+    {
+        public Food() : base()
+        { }
+
+        public override void Draw(double x, double y)
+        {
+            SplashKit.FillRectangle(Color.Yellow, FirstX, FirstY, 15, 15);
+        }
+    }
+}
